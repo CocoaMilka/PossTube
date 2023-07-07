0c2d278cf31b61bebb69b14326755b8146cc4d48
Cocoa Milka <59587889+CocoaMilka@users.noreply.github.com>
2023-07-07 16:04:55 -0500

    test: Attempting to grab thumbnails from youtube

--- posstube.js
+++ posstube.js
@@ -1,1 +1,31 @@
 document.body.style.border = "5px solid red";
+
+// Grab all thumbnails...
+function getThumbnails()
+{
+    var thumbnails = document.getElementById("thumbnail-container");
+    var thumbnailSources = [];
+
+    /* FOR GRABBING IMAGE SOURCE
+    for (var i = 0; i < thumbnails.length; i++)
+    {
+        thumbnailSources.push(thumbnails[i].src);
+    }
+
+    return thumbnailSources;
+    */
+
+    return thumbnails;
+}
+
+function addPoss()
+{
+    var thumbnails = getThumbnails();
+
+    for (var i = 0; i < thumbnails.length; i++)
+    {
+        thumbnails[i].style = "background-color: red;";
+    }
+}
+
+addPoss();
